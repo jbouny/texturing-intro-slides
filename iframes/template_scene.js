b62596cd3b0360ff6f5f05dda89f4b7c2086daa5
Jérémy Bouny <jbouny@gmail.com>
2016-10-08 03:23:50 +0200

    Add basic webgl examples
    - Cube with const color
    - Cube with dynamic color

---
+++ iframes/template_scene.js
@@ -0,0 +1,43 @@
+function create3DScene(config) {
+	var camera, controls, renderer, scene;
+	var startTime = Date.now();
+
+	init();
+	animate();
+
+	function init() {
+		var container = document.createElement('div');
+		document.body.appendChild(container);
+
+		renderer = new THREE.WebGLRenderer({alpha: true});
+		renderer.setPixelRatio(window.devicePixelRatio);
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		container.appendChild(renderer.domElement);
+
+		camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
+		camera.position.z = 2.5;
+		controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+		window.addEventListener('resize', onWindowResize, false);
+
+		scene = new THREE.Scene();
+		config.init(camera, scene);
+	}
+
+	function onWindowResize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+
+	function animate() {
+		requestAnimationFrame(animate);
+		controls.update();
+
+		var time = (Date.now() - startTime) / 1000;
+		config.update(time);
+
+		renderer.render(scene, camera);
+	}
+}

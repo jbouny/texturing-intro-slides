9a533008ebf45353eba25195f9600343e461ec57
Jérémy Bouny <jbouny@gmail.com>
2016-11-17 02:31:44 +0100

    Add basic lighting example

--- iframes/template_scene.js
+++ iframes/template_scene.js
@@ -54,3 +54,72 @@ function loadBase64Texture(data, succeed) {
 	}
 	image.src = data;
 }
+
+function makeSpriteFromText(text, parameters) {
+	// author: stemkoski (https://stemkoski.github.io/Three.js/Sprite-Text-Labels.html)
+	function roundRect(ctx, x, y, w, h, r) 
+	{
+		ctx.beginPath();
+		ctx.moveTo(x+r, y);
+		ctx.lineTo(x+w-r, y);
+		ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+		ctx.lineTo(x+w, y+h-r);
+		ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+		ctx.lineTo(x+r, y+h);
+		ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+		ctx.lineTo(x, y+r);
+		ctx.quadraticCurveTo(x, y, x+r, y);
+		ctx.closePath();
+		ctx.fill();
+		ctx.stroke();  
+	} 
+
+	var defaultParameters = {
+		font: "Arial",
+		size: 28,
+		backgroundColor: [0, 0, 0, 1],
+		textColor: [200, 200, 200, 1],
+		fillColor: [128, 128, 128, 1],
+		borderThickness: 4,
+		borderColor: [200, 200, 200, 1]
+	}
+	if (parameters === undefined) {
+		parameters = {};
+	}
+	for (var key in defaultParameters) {
+		if (!(key in parameters)) {
+			parameters[key] = defaultParameters[key];
+		}
+	}
+
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	context.font = "Bold " + parameters.size + "px " + parameters.font;
+	var metrics = context.measureText(text);
+	context.canvas.width = 512;
+	context.canvas.height = 64
+	var textWidth = metrics.width;
+
+	context.fillStyle = "rgba(" + parameters.backgroundColor.join(',') + ")";
+	context.strokeStyle = "rgba(" + parameters.borderColor.join(',') + ")";
+	context.lineWidth = parameters.borderThickness;
+	roundRect(
+		context,
+		(context.canvas.width - parameters.borderThickness - textWidth) * 0.5,
+		(context.canvas.height - parameters.size * 1.4 - parameters.borderThickness) * 0.5,
+		textWidth + parameters.borderThickness,
+		parameters.size * 1.4 + parameters.borderThickness,
+		8);
+
+	context.textAlign = 'center';
+	context.fillStyle = "rgba(" + parameters.textColor.join(',') + ")";
+	context.font = "Bold " + parameters.size + "px " + parameters.font;
+	context.fillText(text, context.canvas.width  * 0.5, (context.canvas.height  + parameters.size * 0.7) * 0.5); 
+	
+	var texture = new THREE.Texture(canvas);
+	texture.needsUpdate = true;
+	var sprite = new THREE.Sprite(new THREE.SpriteMaterial({map: texture}));
+	sprite.scale.set(1.0, 1.0 * context.canvas.height / context.canvas.width, 1);
+	return sprite;
+}
+
